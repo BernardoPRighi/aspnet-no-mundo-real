5aa8bcd259d6476352f4c32954392af2b7cd9579
BernardoPRighi <bernardo.righi@tramontina.com>
2021-11-24 14:12:45 -0300

    Teste

--- Aula01-Samples/AplicativoEscolas.WebApi/Controllers/AlunosController.cs
+++ Aula01-Samples/AplicativoEscolas.WebApi/Controllers/AlunosController.cs
@@ -14,6 +14,7 @@ namespace AplicativoEscolas.WebApi.Controllers
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Situacao { get; set; }
+        public string Situacao2 { get; set; }
     }
 
     public interface IAlunosDataAccess
